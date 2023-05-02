e079a731d0f04bf3d6dc0bedefb2b70bc6e87a8e
sabayarw <sabayarw@gmail.com>
2023-05-02 16:38:18 -0300

    carpeta variables

---
+++ variables.js
@@ -0,0 +1,20 @@
+// Nombre de variable
+var cajon1;
+// valor de la variable
+//Strings " "
+var cajon1; "guantes"
+//Numeros
+var numeroEntero: 17;
+var numeroNegativo: -2;
+var numeroDecimal: 11.75;
+//Booleanos: V / F No van entre comillas
+var EstoyEntendiendo = true;
+var EstoyAburrido = false;
+// Undefined: cuando aun no hemos definido a la variable (var cajon1) y el sistema lo coloca como undefined
+var cajon1;
+console.log(cajon1);
+//Null: similar a undefined pero nosotros somos quienes le ponemos el valor null
+var sinNada=null;
+
+
+
